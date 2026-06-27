9e58685546bc71fe0688a4b804f268fff5ed3184
liftedking <liftedkinggaza@gmail.com>
2026-06-27 17:23:55 +0000

    reuploaded again

--- pages/api/auth/pinterest/login.js
+++ pages/api/auth/pinterest/login.js
@@ -1,4 +1,4 @@
-import { getPinterestAuthUrl } from "../../../lib/pinterest";
+import { getPinterestAuthUrl } from "../../../../lib/pinterest";
 
 export default function handler(req, res) {
   if (req.method !== "GET") return res.status(405).end();
